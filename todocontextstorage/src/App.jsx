79d542a1b3ee9159980b9a0b7726f49b340046dd
prakhar <prakharharshit.2003@gmail.com>
2024-05-28 18:03:41 +0530

    Basis functionality of context added

---
+++ todocontextstorage/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import {TodoProvider} from './contexts'
+import './App.css'
+
+function App() {
+  //todos is an array which will contain many values;
+  //see notebook for updateTodo explanation
+  const [todos, setTodos] = useState([])
+const addTodo=(todo)=>{
+setTodos((prev)=>[{id:Date.now(),...todo},...prev])
+}
+//see notebook for updateTodo explanation
+const updateTodo=(id,todo)=>{
+  setTodos((prev)=>
+  prev.map((prevTodo)=>
+  (prevTodo.id===id ? todo:prevTodo))
+  )
+}
+
+const deleteTodo=(id)=>{
+  setTodos((prev)=>prev.filter((prevTodo)=>prevTodo.id!==id))
+}
+//  completed woh hai jo Todocontext mai hai Todos mai hai true or false
+const toggleTodo=(id)=>{
+  setTodos((prev)=>prev.map((prevTodo)=>prevTodo.id===id ? {...prevTodo,completed:!prevTodo.completed }:prevTodo))
+}
+
+
+  return (
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleTodo}}>
+    
+    <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+    </TodoProvider>
+  )
+}
+
+export default App
---
+++ todocontextstorage/src/contexts/index.js
@@ -0,0 +1,3 @@
+export {TodoContext,TodoProvider,useTodo} from './TodoContext';
+
+//useTodo mai context hai issliye ussme mai sb hm filter krke bahar nikal lege.
