850d7ec90624e212122a564264f95d36e95b2bfd
prakhar <prakharharshit.2003@gmail.com>
2024-05-30 23:32:18 +0530

    complete react redux todo

---
+++ reduxToolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,50 @@
+import {createSlice,nanoid} from '@reduxjs/toolkit';
+
+const initialState={
+    todos:[
+        {
+            id:1,
+            text:"jai shree ram",
+
+        }
+    ]
+}
+
+
+export const todoSlice=createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+//we can write these fn in eperate file and import it
+        addTodo:(state,action)=>{
+            const todo={
+                id:nanoid(),
+                text:action.payload,
+            } 
+            state.todos.push(todo);
+
+        },
+        removeTodo:(state,action)=>{
+            state.todos=state.todos.filter((todo)=>
+                todo.id!==action.payload
+
+            )
+
+
+        },
+        updateTodo:(state,action)=>{
+            const {id,text}=action.payload;
+            // find todo by its id and update the text provided in action payload 
+            const existingTodo=state.todos.find((todo)=>todo.id===id);
+            if(existingTodo){
+                existingTodo.text=text;
+            }
+
+        }
+
+    }
+
+})
+export const {addTodo,removeTodo,updateTodo}=todoSlice.actions;
+ export default todoSlice.reducer;
+ 
